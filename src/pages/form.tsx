3de036b2503b95e961d090898b5d524dd71aef80
alorslouis <alorslouis@users.noreply.github.com>
2022-07-26 20:23:22 -0700

    +: tailwind forms & basic impl

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -3,6 +3,7 @@ import { useForm, SubmitHandler } from "react-hook-form";
 
 type Inputs = {
   example: string;
+  yalla: string;
   exampleRequired: string;
 };
 
@@ -19,9 +20,25 @@ export default function App() {
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col p-4 mx-auto text-center border border-solid border-gray-300 rounded-lg shadow-lg"
+    >
       {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+      <label className="px-4">
+        test1
+        <input
+          defaultValue="test"
+          {...register("example")}
+          className="form-input mx-4 px-4 py-3 rounded-lg"
+        />
+      </label>
+      <input
+        defaultValue="yalla"
+        {...register("yalla")}
+        className="form-input mx-4 px-4 py-3 rounded-lg"
+      />
+      <select />
 
       {/* include validation with required or other standard HTML validation rules */}
       <input {...register("exampleRequired", { required: true })} />
