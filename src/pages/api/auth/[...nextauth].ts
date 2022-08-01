5d943e0fc7da8b4d1a31af5c34b9ec0dd6bfd1c3
alorslouis <alorslouis@users.noreply.github.com>
2022-08-01 00:18:02 -0700

    fkn pos

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,7 +11,7 @@ export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
     session({ session, user }) {
-      if (session.user) {
+      if (session?.user) {
         session.user.id = user.id;
       }
       return session;
